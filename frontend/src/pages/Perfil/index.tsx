da8c227d924bb83f13ec247e9553d34c3e449320
Diego Pontes de Carvalho <diego.pontes@zhaz.com.br>
2024-11-22 16:41:55 -0300

    v1.1-comecando_tela_perfil-22/11/2024

--- frontend/src/pages/Perfil/index.tsx
+++ frontend/src/pages/Perfil/index.tsx
@@ -2,10 +2,28 @@
 import { IconPerfil } from '@/components/icons/IconesMaterial';
 import Layout from './../../components/template/Layout';
 
-export default function Perfil(){
-    return(
-        <Layout icone={<IconPerfil fontSize='large'/>} texto="Perfil">
-            <div>Perfil</div>
+export default function Perfil() {
+    return (
+        <Layout icone={<IconPerfil fontSize='large' />} texto="Perfil">
+            <div className="flex max-sm:flex-col-reverse h-full">
+                <div className='border flex w-[70%] max-sm:w-[100%] max-sm:h-[70%]'>
+                    <div className='bg-white flex w-[100%] shadow-lg rounded-md'>
+                        5
+                    </div>
+                </div>
+                <div className='flex flex-col w-[30%] max-sm:flex-row max-sm:w-[100%] max-sm:h-[30%]'>
+                    <div className='border flex w-[100%] h-[50%] max-sm:h-[100%]'>
+                        <div className='bg-white flex flex-col h-[100%] w-[100%] shadow-lg rounded-md'>
+                            5
+                        </div>
+                    </div>
+                    <div className='border flex w-[100%] h-[50%] max-sm:h-[100%]'>
+                        <div className='bg-white flex w-[100%] shadow-lg rounded-md'>
+                            5
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Layout>
     )
 }
